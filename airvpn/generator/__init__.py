4475ec19788e1c0429c83b690613b85b0f5ac85a
ch0nker <chonkers.git@gmail.com>
2026-07-20 05:23:04 -0700

    [fix]: use of object before declaration

--- airvpn/generator/__init__.py
+++ airvpn/generator/__init__.py
@@ -161,7 +161,7 @@ class Generator:
                     wireguard_persistent_keepalive: int = 15,
                     iplayer_entry: str = "ipv4",
                     iplayer_exit: str = "both",
-                    **kwargs: dict) -> ConfigList | str:
+                    **kwargs: dict) -> 'ConfigList' | str:
             """Generate VPN configuration file(s) for one or more servers.
  
             Requests the configuration generator with `download="auto"` and returns the
