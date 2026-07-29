cdd5167625ec037748507cfd3f72adafd6fe5777
ch0nker <chonkers.git@gmail.com>
2026-07-29 07:54:54 -0700

    [docs]: add docstrings for enums

--- airvpn/web/auth/models.py
+++ airvpn/web/auth/models.py
@@ -1,6 +1,18 @@
 from enum import IntEnum
 
 class ProfilePrivacy(IntEnum):
+    """Controls who is allowed to view a user's profile.
+
+    Used with `AuthUser.edit_profile` to set the
+    `air_ipb_profile_privacy_title` field on the profile edit form.
+
+    Attributes:
+        ALL: Profile is visible to everyone, including guests.
+        FOLLOWED_MEMBERS: Profile is visible only to members the user follows.
+        COMMUNITY_MEMBERS: Profile is visible only to registered/logged-in
+            community members.
+        PRIVATE: Profile is visible only to the user themselves.
+    """
     ALL = 1
     FOLLOWED_MEMBERS = 2
     COMMUNITY_MEMBERS = 3
